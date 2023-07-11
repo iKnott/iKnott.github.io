d3ecdd5b1da0597e41f6fc60e23a6f1cc8a589a7
Sagnik Sarkar <sagniksarkar.agt@gmail.com>
2023-07-11 22:16:27 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", { 
-        strings: ["Developer", "Engineer", "Swimmer", "Painter","Analyst"],
+        strings: ["Solution Consultant", "Solution Developer", "Data Analyst", "Fullstack Dev","DevOps Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Engineer", "Swimmer", "Painter","Analyst"],
+        strings: ["Solution Consultant", "Solution Developer", "Data Analyst", "Fullstack Dev","DevOps Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
